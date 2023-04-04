03da35dbe66b3e6012bbc35db271bbbc4b092451
Antu <antu99gayen@gmail.com>
2023-04-04 21:34:02 +0530

    timestamp microservice created

--- index.js
+++ index.js
@@ -21,12 +21,33 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({greeting: new Date()});
+});
+
+app.get("/api/:date", function (req, res) {
+  let date;
+  if(req.params.date){
+    date = new Date(req.params.date);
+  } else {
+    date = new Date();
+  }
+
+  if(date) {
+    let obj = {};
+    let dateType = date.split(',');
+    if(dateType.length > 0){
+      obj.unix = req.params.date;
+      obj.utc = date;
+    }    
+    return res.json(obj);
+  } else {
+    return res.json({ error: "Invalid Date" });
+  }
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
